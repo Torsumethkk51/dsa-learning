e59910631806e6f1293c2398fd2bc52f62f45ee2
Torsumethkk51 <torsumethkk.hoyo@gmail.com>
2026-07-25 18:09:52 +0700

    feat(array): add quicksort

---
+++ array/learning/quicksort/quicksort.c
@@ -0,0 +1,41 @@
+#include "../../lib/array_lib_head.h"
+#include <stdio.h>
+
+void quicksort(int *arr, int start, int end) {
+  int ipivot = end - 1;
+  int pivot = arr[ipivot];
+  int i = start - 1;
+
+  for (int j = 0; j < ipivot; j++) {
+    if (arr[j] < pivot) {
+      i++;
+      int temp = arr[j];
+      arr[j] = arr[i];
+      arr[i] = temp;
+    }
+  }
+
+  int temp = arr[i + 1];
+  arr[i + 1] = pivot;
+  arr[ipivot] = temp;
+
+  ipivot = i + 1;
+
+  quicksort(arr, 0, ipivot - 1);
+  quicksort(arr, ipivot + 1, end);
+}
+
+int main() {
+  int n = 20;
+  int *arr = gen_int_array(n, (random_range) { 0, 1000 });
+
+  printf("Before sort:\n");
+  print_int_array(arr, n);
+
+  quicksort(arr, 0, n);
+
+  printf("After sort:\n");
+  print_int_array(arr, n);
+
+  return 0;
+}
